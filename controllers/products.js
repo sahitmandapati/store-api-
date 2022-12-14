8064f91e17c0d08ce859af9bfcf1596b537bcde6
sahitmandapati <sahit.m@technovert.com>
2022-12-14 11:42:34 +0530

    update - controllers/products
    setup logic to display products based on query changes in URL

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,34 @@
-const getAllProductsStatic = async (req,res) => {
-    res.status(200).json({msg : 'product testing route'})
-}
+const Product = require("../models/product");
 
-const getAllProducts = async (req,res) => {
-    res.status(200).json({msg : 'product route'})
-}
+const getAllProductsStatic = async (req, res) => {
+  const search = 'ab'
+  const products = await Product.find({ name : { $regex: search, $options: 'i' }});
+  res.status(200).json({ products, nhHits: products.length });
+};
 
-module.exports = {getAllProductsStatic,getAllProducts}
+const getAllProducts = async (req, res) => {
+  const { featured , company , name } = req.query;
+
+  const queryObject = {};
+
+  //featured is string 
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company){
+    queryObject.company = company
+  }
+  
+  if(name){
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+
+
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nhHits: products.length });
+};
+
+module.exports = { getAllProductsStatic, getAllProducts };
