6e3dd406c55444fd03f70ee4f5e3892b0aac9f98
sahitmandapati <sahit.m@technovert.com>
2022-12-14 14:51:13 +0530

    Update - products.js
    set up sorting , display certain filters and also page and limits

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,13 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab'
-  const products = await Product.find({ name : { $regex: search, $options: 'i' }});
+  const products = await Product.find({}).sort().select('name price')
+  
   res.status(200).json({ products, nhHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured , company , name } = req.query;
+  const { featured , company , name , sort , fields  } = req.query;
 
   const queryObject = {};
 
@@ -27,7 +27,34 @@ const getAllProducts = async (req, res) => {
 
 
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+
+  if(sort){
+    // sort = 'name,-price'
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  }else{
+    result = result.sort('createdAt')
+  }
+
+  if(fields){
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+
+
+  const skip = (page-1) * limit
+
+  result = result.skip(skip).limit(limit)
+  // 23 products
+  // 4 - pages -> 7 7 7 2
+   
+ 
+  const products =  await result
+
   res.status(200).json({ products, nhHits: products.length });
 };
 
