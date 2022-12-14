a2608ecb52966b4a63701e20cde934dc76bee4a3
sahitmandapati <sahit.m@technovert.com>
2022-12-14 15:22:51 +0530

    update - products.js
    filter functionality on price and rating

--- controllers/products.js
+++ controllers/products.js
@@ -1,59 +1,79 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort().select('name price')
-  
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("price")
+    .select("name price");
+
   res.status(200).json({ products, nhHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured , company , name , sort , fields  } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
 
   const queryObject = {};
 
-  //featured is string 
+  //featured is string
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
 
-  if (company){
-    queryObject.company = company
-  }
-  
-  if(name){
-    queryObject.name = { $regex: name, $options: 'i' }
+  if (company) {
+    queryObject.company = company;
   }
 
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
 
+  if (numericFilters) {
+    const opertatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${opertatorMap[match]}-`
+    );
+    const options = ['price','rating']
+    filters = filters.split(',').forEach((item) => {
+        const [field, operator , value] = item.split('-')
+        if(options.includes(field)){
+            queryObject[field] = {[operator] : Number(value)}
+        }
+    });
+  }
 
   let result = Product.find(queryObject);
 
-  if(sort){
+  if (sort) {
     // sort = 'name,-price'
-    const sortList = sort.split(',').join(' ')
-    result = result.sort(sortList)
-  }else{
-    result = result.sort('createdAt')
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
   }
 
-  if(fields){
-    const fieldsList = fields.split(',').join(' ')
-    result = result.select(fieldsList)
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
   }
 
-  const page = Number(req.query.page) || 1
-  const limit = Number(req.query.limit) || 10
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
 
+  const skip = (page - 1) * limit;
 
-  const skip = (page-1) * limit
-
-  result = result.skip(skip).limit(limit)
+  result = result.skip(skip).limit(limit);
   // 23 products
   // 4 - pages -> 7 7 7 2
-   
- 
-  const products =  await result
+
+  const products = await result;
 
   res.status(200).json({ products, nhHits: products.length });
 };
